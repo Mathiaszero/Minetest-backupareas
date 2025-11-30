3773a6a2508f5341bb201c5eea110190e74207f2
Mathiaszero <55817193+Mathiaszero@users.noreply.github.com>
2025-11-29 20:18:39 -0500

    Comment out unused functions and clean up code
    Comment out the 'fix_lines' function and related code, and remove unused code for saving nodes and loading areas.

--- backupareas/commands.lua
+++ backupareas/commands.lua
@@ -1,5 +1,27 @@
 local S = core.get_translator("backupareas")
 
+local function fix_lines(schema_dir, dir_list)
+	-- for file_index, file in ipairs(dir_list) do
+	-- 	--delete empty lines
+	-- 	local schema_file, err = io.open(schema_dir..file, "a")
+	-- 	local lines = schema_file:lines()
+	-- 	for line_index, line in ipairs(lines) do
+	-- 		if line ~= "" and string.sub(line, -1) ~= "}" then
+	-- 			schema_file.write
+	-- 		end
+	-- 	end
+	-- 	schema_file:close()
+		
+	-- 	-- local schema_file, err = io.open(schema_dir..file, "a")
+	-- 	-- local lines = schema_file:lines()
+	-- 	-- for line_index, line in ipairs(lines) do
+	-- 	-- 	if line ~= nil and string.sub(line, -1) ~= "}"
+	-- 	-- 		count = count + 1
+	-- 	-- end
+	-- 	-- schema_file:close()
+	-- end
+end
+
 local function save_node(name, schema_file, x, y, z)
 	--create list to hold node data
 	local node = {}
@@ -27,10 +49,27 @@ local function save_node(name, schema_file, x, y, z)
 
 	node["n_meta"] = {}
 	for i, v in ipairs(node_meta_keys) do
-		if node_table.name == "default:furnace" and v == "infotext" then
-			--return
-		end
-		node["n_meta"][v] = node_meta:get_string(v)
+		-- if node_table.name == "default:furnace" and v == "infotext" then
+		-- 	--return
+		-- end
+		-- node["n_meta"][v] = node_meta:get_string(v)
+
+		-- if node_table.name == "default:furnace" and v == "infotext" then
+		-- 	--
+		-- else
+		-- 	node["n_meta"][v] = node_meta:get_string(v)
+		-- end
+
+		--local single_line = string.gsub(v, "\n", " ")
+		local meta_val = node_meta:get_string(v)
+		local meta_val_line = string.gsub(meta_val, "\n", " ")
+		node["n_meta"][v] = meta_val_line
+
+
+		-- if string.find(v, "\n") ~= nil then
+			
+		-- end
+
 		--node["node_meta"][v] = "hi"
 	end
 
@@ -153,6 +192,7 @@ core.register_chatcommand("sa", {
 			end	
 			schema_file:close()
 		end
+		fix_lines(schema_dir, dir_list)
 	end,
 })
 
@@ -170,168 +210,56 @@ core.register_chatcommand("la", {
 			local schema_file, err = io.open(schema_dir..file, "r")
 			--loop through each node data which is a line in schema file
 			for line in schema_file:lines() do
-				obj = core.deserialize(line)
+				--core.chat_send_player(name, line)
+				local obj = core.deserialize(line)--ok
+				if obj then
+					--core.chat_send_player(name, obj[1])
+					--local json = core.parse_json(obj)--string expected got table error
+					--core.chat_send_player(name, dump(obj))
+
+					local x = obj[1]
+					local y = obj[2]
+					local z = obj[3]
+					local pos = vector.new(x, y, z)
+					local node_table = {name = obj[4], param1 = obj[5], param2 = obj[6]}
+					core.set_node(pos, node_table)
+
+					local node_meta = core.get_meta(pos)
+					local node_meta_keys = obj["n_meta"]
+					for key, value in pairs(node_meta_keys) do
+						--core.chat_send_player(name, i..","..v)
+						node_meta:set_string(key, value)
+					end
+
+					local inv = node_meta:get_inventory(pos)
+
+				end
+				--core.chat_send_player(name, obj[1])--if enabled it errors
+				
+				--core.chat_send_player(name, dump(obj["inv"]))
+
+				-- if line then
+				-- 	local obj = core.deserialize(line)
+				-- 	core.chat_send_player(name, obj[1])
+				-- end
+
+				--local json = core.parse_json(obj)
 				--core.chat_send_player(name, obj[1])
-				--core.set_node()
+				-- local x = obj[1]
+				-- local y = obj[2]
+				-- local z = obj[3]
+				-- local pos = vector.new(x, y, z)
+				-- local node_table = {name = obj[4], param1 = obj[5], param2 = obj[6]}
+				-- core.set_node(pos, node_table)
+				--local node_meta = core.get_meta(pos)
+
+				
 			end
 			--close schema file after all nodes and their data are set in world
 			schema_file:close()
 		end
 	end,
 })
-
--- --random hex separator instead of commas, so sign/book text can be added
--- local divider = "|2ef9c|"
-
--- local function monitor(i, total, x, y, z, xmax, ymax, zmax)
--- 	local monitor_file, err = io.open(core.get_worldpath().."/monitor.txt", "w")
--- 	monitor_file:write(i.."/"..total..": "..x..","..y..","..z.."/"..xmax..","..ymax..","..zmax)
--- 	monitor_file:close()
--- end
-
--- local function save_node(name, schema_file, x, y, z)
--- 	--open file to append node data (lags b/c each node)
--- 	--local schema_file, err = io.open(schema_dir..file, "a")
-	
--- 	--create list to hold node data
--- 	local node = {}
--- 	--insert each coordinate into node data list
--- 	table.insert(node, x)
--- 	table.insert(node, y)
--- 	table.insert(node, z)
--- 	--combine coordinates into vector position
--- 	local pos = vector.new(x, y, z)
--- 	--get node name, lighting and orientation data
--- 	local node_table = core.get_node(pos)
--- 	--insert data into node data list
--- 	table.insert(node, node_table.name)
--- 	table.insert(node, node_table.param1)
--- 	table.insert(node, node_table.param2)
--- 	--get node metadata
--- 	local node_meta = core.get_meta(pos)
--- 	local node_meta_keys = node_meta:get_keys()
-
--- 	if #node_meta_keys > 0 then
--- 		--core.chat_send_player(name, dump(node_meta_keys))
--- 	end
-
--- 	--[7]
--- 	if node_table.name == "default:chest_locked" then
--- 		table.insert(node, node_meta:get_string("owner"))
--- 	elseif node_table.name == "default:sign_wall_wood" then
--- 		table.insert(node, node_meta:get_string("text"))
--- 	end
-
--- 	--get node inventory metadata
--- 	local inv = node_meta:get_inventory()
--- 	--get inventory types
--- 	for list, items in pairs(inv:get_lists()) do
--- 		--for each inventory type, loop through slots
--- 		for i, itemstack in ipairs(items) do
--- 			--if slot empty, insert placeholder, b/c data organized as [list] [name] [count]
--- 			if itemstack:is_empty() then
--- 				table.insert(node, "0 0 0 0 0")
--- 			else
--- 				local itemstack_meta = itemstack:get_meta()
--- 				local itemstack_meta_keys = itemstack_meta:get_keys()
--- 				--local book_body = {}
--- 				local book_body = ""
--- 				for i, v in ipairs(itemstack_meta_keys) do
--- 					-- if i == #itemstack_meta_keys then
--- 					-- 	table.insert(book_body, v)
--- 					-- end
--- 					--book_body = book_body..v
--- 				end
-
--- 				table.insert(
--- 					node, 
--- 					list.." "..
--- 					itemstack:get_name().." "..
--- 					itemstack:get_count()
--- 					--"_"..itemstack_meta:get_string("description").."|"..
--- 					--"_"..itemstack_meta:get_string("text")
--- 				)
--- 			end
--- 		end
--- 	end
--- 	--write node data as comma separated line in schema file
--- 	schema_file:write(table.concat(node, divider).."\n")
-	
--- 	--close schema file after writing in all data for that node 
--- 	--schema_file:close()
--- end
-
--- core.register_chatcommand("sa", {
--- 	description = S("Save areas."),
--- 	privs = {server = true,},
--- 	func = function(name, param)
--- 		--create folder to store areas as text files
--- 		local schema_dir = core.get_worldpath().."/schema/"
--- 		--cleanup directory to delete areas that no longer exist
--- 		core.rmdir(schema_dir, true)
--- 		--recreate directory
--- 		core.mkdir(schema_dir)
--- 		--get list of areas in areas mod data file
--- 		local ad_path = core.get_worldpath().."/areas.dat" 
--- 		--open areas dat file
--- 		local ad_file, err = io.open(ad_path, "r")
--- 		--read all text in areas dat file
--- 		local ad_data = ad_file:read("*all")
--- 		--text in areas dat file can be parsed to json
--- 		local ad_json = core.parse_json(ad_data)
--- 		--no areas shows as "null" in areas dat file, so catch it
--- 		if ad_json == nil then
--- 			core.chat_send_player(name, "Error, no areas to save.")
--- 			ad_file:close()
--- 			return
--- 		end
--- 		--loop through each areas dat entry to get area bounds pos1 and pos2
--- 		for i, v in ipairs(ad_json) do
--- 			--get pos1 coordinates
--- 			local p1x , p1y, p1z = v["pos1"]["x"], v["pos1"]["y"], v["pos1"]["z"]
--- 			--get pos2 coordinates
--- 			local p2x , p2y, p2z = v["pos2"]["x"], v["pos2"]["y"], v["pos2"]["z"]
--- 			--format coordinates as list for schema file name
--- 			local schema_suffix = string.format(
--- 				"%s,%s,%s,%s,%s,%s,.txt",
--- 				p1x,p1y,p1z,p2x,p2y,p2z
--- 			)
--- 			--create empty schema file with suffix name
--- 			local schema_file, err = io.open(schema_dir..schema_suffix, "w")
--- 			--close schema file for use later
--- 			schema_file:close()
--- 		end
--- 		--close areas dat file since all schema files are created
--- 		ad_file:close()
--- 		--get list of files in schema folder
--- 		local dir_list = core.get_dir_list(schema_dir, false)
--- 		--loop through each schema file name
--- 		for i, file in ipairs(dir_list) do
--- 			--split the schema file name based on ","
--- 			local fns = string.split(file, ",")
--- 			--first 3 are pos1 coordinates
--- 			local p1x, p1y, p1z = fns[1], fns[2], fns[3]
--- 			--last 3 are pos2 coordinates
--- 			local p2x, p2y, p2z = fns[4], fns[5], fns[6]
--- 			--determine min coordinates to loop through area nodes
--- 			local xmin, ymin, zmin = math.min(p1x, p2x), math.min(p1y, p2y), math.min(p1z, p2z)
--- 			--determine max coordinates to loop through area nodes
--- 			local xmax, ymax, zmax = math.max(p1x, p2x), math.max(p1y, p2y), math.max(p1z, p2z)
--- 			--loop through each node within area bounds
--- 			local schema_file, err = io.open(schema_dir..file, "a")
--- 			for x = xmin, xmax do
--- 				for y = ymin, ymax do
--- 					for z = zmin, zmax do
--- 						--monitor(i, #dir_list, x, y, z, xmax, ymax, zmax)
--- 						--save_node(schema_dir, file, x, y, z)
--- 						save_node(name, schema_file, x, y, z)
--- 					end
--- 				end
--- 			end	
--- 			schema_file:close()
--- 		end
--- 	end,
--- })
 
 -- core.register_chatcommand("la", {
 -- 	description = S("Load areas."),
